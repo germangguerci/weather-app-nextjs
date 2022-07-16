3c323bb80d3b7e3e95a03a4dbc8bb6fd6725ce61
German <germangguerci@gmail.com>
2022-07-16 19:45:56 -0300

    one-call api endpoint created

---
+++ pages/api/openweathermap/one-call.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+export default async function handler(req, res) {
+  const { query: { lat, lon } } = req;
+
+  const API_KEY = process.env.WEATHER_API_KEY;
+
+  const apiResponse = await axios.get(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&exclude={current, minutely, alerts}&appid=${API_KEY}`)
+    .then((res) => {
+      return res?.data;
+    })
+    .catch((err) => {
+      return err?.response?.data;
+    });
+
+  res.status(apiResponse?.cod).json(apiResponse);
+}
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,19 @@
 import Head from 'next/head';
 import { motion } from 'framer-motion';
 
+import {
+  useQuery,
+  useQueryClient,
+} from 'react-query';
+import axios from 'axios';
+
 export default function Home() {
+
+  const queryClient = useQueryClient();
+
+  const { isLoading, error, data } = useQuery(['oneCall', 'test'],
+    async () => axios.get('/api/openweathermap/one-call/?lat=-22.908333&lon=-43.196388'));
+
   return (
     <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} key="home" style={{ width: '100%' }}>
       <Head>
