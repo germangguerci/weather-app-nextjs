ee81f149ae3a468129987357bc4b28e65822fd41
German <germangguerci@gmail.com>
2022-07-17 11:38:59 -0300

    react query defaultOptions updated

--- components/public/CityWeather.js
+++ components/public/CityWeather.js
@@ -1,16 +1,16 @@
 import { useEffect } from 'react';
 import { useQuery } from 'react-query';
+import getCityWeather from '../../utils/getCityWeather';
 
 export default function CityWeather({ lat, lon }) {
 
-  /*  const { isLoading, error, data } = useQuery(['getCityWeather', { lat, lon }], async () => getCityWeather(lat, lon)); */
+  const { isLoading, error, data } = useQuery(['getCityWeather', { lat, lon }], async () => getCityWeather(lat, lon));
 
   return (
-    <p>{lat}</p>
-    /*  <div>
-       {isLoading && "loading"}
-       {error && "error"}
-       {data && "data loaded"}
-     </div> */
+    <div>
+      {isLoading && "loading"}
+      {error && "error"}
+      {data && "data loaded"}
+    </div>
   );
 }
--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,15 @@ import { QueryClientProvider, QueryClient } from 'react-query';
 import WheaterContextProvider from "../context/WeatherContext";
 import GlobalStyles from '../configs/theme';
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      staleTime: (1000 * 60 * 60 * 12),
+      cacheTime: (1000 * 60 * 60 * 24)
+    },
+  },
+});
 
 
 function MyApp({ Component, pageProps }) {
